43e634836f68ef7ee7f6511fa83fa7d1e7451248
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-01-31 11:46:51 -0600

    i add another section of the calculator

--- class12-materials/simple-calculator/js/main.js
+++ class12-materials/simple-calculator/js/main.js
@@ -4,6 +4,7 @@ document.querySelector('#pumpkin').addEventListener('click', makeZero)
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector('#plus').addEventListener('click', added)
 
 function makeZero() {
   total = 0
@@ -22,5 +23,10 @@ function add9() {
 
 function sub2() {
   total = total - 2
+  document.querySelector('#placeToPutResult').innerHTML = total
+}
+
+function added() {
+  total = total + 10
   document.querySelector('#placeToPutResult').innerHTML = total
 }
