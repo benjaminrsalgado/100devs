60aeaacd20c2e877d42268db8cd23fa930335b36
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-02-09 17:11:41 -0600

    i add a convert excersice

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,17 @@
 //Write your pseduo code first! 
+//0 - 32
+document.querySelector('#yell').addEventListener('click', convert)
+
+
+function convert() {
+    
+
+
+    //Need the value that is in selseyus
+    let temp = document.querySelector('#sel').value
+   //Need to convert it, from selseyus to farrinheight 
+    temp = temp * 9/5 + 32 
+   // show it 
+    document.querySelector('#placeToYell').innerText = temp 
+
+}
