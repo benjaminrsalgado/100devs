8cc8971dbb5f662d1adeb8fa57f49cffcb2e25f4
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-02-09 18:18:44 -0600

    i did bachelor 3

--- class13-materials/bachelor-code/3/js/main.js
+++ class13-materials/bachelor-code/3/js/main.js
@@ -1,10 +1,11 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+//'.contestant el la clase del h2 llamado Claire// 
+const contestants = document.querySelectorAll('.contestant')
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
 
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains('rose')){
+		document.querySelector('#nikki').classList.toggle('hidden')
 	}else{
 		alert("Wrong!");
 	}
