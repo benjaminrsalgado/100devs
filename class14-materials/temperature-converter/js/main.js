92dfb714d36025153f2cc81c3446c456442d75cb
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-03-12 17:12:01 -0600

    i add pseudo code to my JS

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
 //Write your pseduo code first! 
+
+// only on click
+document.querySelector('#yell').addEventListener('click', run)
+
+function run(){
+    const fName = document.querySelector('#firstName').value
+
+    //Get value out of the input 
+}
