a1cf19a417c1093da627c8fe7880a83206890ac5
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-03-14 14:50:20 -0600

    i add some functions in the js

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,19 @@
 //create a function that adds two numbers and alerts the sum
-
+function addTwoNumbers(a, b) {
+    const sum = a + b;
+    alert(sum);
+}
 //create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(a, b, c) {
+    const product = a * b * c;
+    console.log(a * b * c);
+}
+
+multiplyThreeNumbers (2, 3, 4)
 
 //create a function that divides two numbers and returns the ???
+function divideTwoNumbers(a, b) {
+    return a / b;
+}
+
+console.log (divideTwoNumbers: (12, 16) )
