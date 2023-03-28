6fe1eaedc9dabdfd9086760c1efb038b6e7c45c2
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-03-28 13:42:24 -0600

    i did some changes

--- class16-materials/simple-functions/js/main.js
+++ class16-materials/simple-functions/js/main.js
@@ -22,6 +22,9 @@ alert (product)
     function remainder (num1, num2, num3){
         return (num1+num2)%num3
     }
+    
+    console.log (remainder (10, 5, 3))
+
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
 function fourNums (num1, num2, num3, num4){
