6af7b891b73a8746d3aa334b7127eeeb4a0a4819
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-04-21 12:22:24 -0600

    i add the class super review

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -5,27 +5,7 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
-add
-let age = 17
 
-if (age < 16) {
-    console.log("You can not drive")
-
-}else if (age < 18) {
-    console.log("You can not hate from outside the club, because you can not even get in")
-
-}else if (age < 21) {
-    console.log("You can not drink")
-
-}else if (age < 25) {
-    console.log("You can not rent cars affordably")
-
-}else if (age < 30) {
-    console.log("You can not rent fancy cars affordably")
-
-}else if (age >= 30) {
-    console.log("There is nothing left to look forward to")
-}
 
 //--- Harder
 //On click of the h1
