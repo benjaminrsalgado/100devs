6a76a07793e76044fc28078c6084cd002bc3c681
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-05-04 13:40:43 -0600

    i add more functions

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
 //Write your pseduo code first! 
+document. querySelector('h1') .addEventListener('click', convert)
+
+function convert (){
+// Need the value
+let enteredVal = Number ( document. querySelector ('input') . value)
+// Convert value c to f
+let temp = enteredVal * 1.8 +  32
+//Show the value to the user 
+document.querySelector ('h2'). innerText = temp
+}
