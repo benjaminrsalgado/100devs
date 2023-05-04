3c62062f2d8b189c99b619a52f87d8dd6770d518
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-05-04 11:58:39 -0600

    i did the excersie 2

--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -1,17 +1,17 @@
 //--- Easy
 //create a variable and assign it a number
-
+let number = 10
 //minus 10 from that number
-
+number = number - 10
 //print that number to the console
-
+console.log(number)
 //--- Medium
 //create a variable that holds a value from the input
-
+let input = document.querySelector('input').value
 //add 25 to that number
-
+input += 25
 //alert that number
-
+alert(input)
 //--- Hard
 //create a variable that holds the h1
 
