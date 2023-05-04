8a4190de9b1334736ced5deb885494e8c30f983b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-05-04 14:45:53 -0600

    i did variables and functions

--- super-review/bring-it-on/js/main.js
+++ super-review/bring-it-on/js/main.js
@@ -1,17 +1,40 @@
 // *Variables*
 // Create a variable and console log the value
-
+let almost = 5
+console.log(almost)
 // Create a variable, add 10 to it, and alert the value
-
+let num = 55 
+num +=10
+alert (num)
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function sub (num1,num2,num3,num4){
+    alert(num1-num2-num3-num4)
+}
 // Create a function that divides one number by another and returns the remainder
-
+function divide (n1,n2){
+    return n1 % n2
+}
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function greater (n1,n2){
+    let sum = n1 + n2
+    if (sum > 50){
+        alert('jumanji')
+    }
+}
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
-
+function mul (n1,n2,n3){
+    let product = n1 * n2* n3
+    if (product %3 ===0){
+        alert('zebra')
+    }
+}
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+function log (word, num )
+for (let i = (method) console.log (...data: any[]):void 
+console.log(word)
+// create a function that divides two numbers   
+
+
