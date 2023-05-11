05e09b3d9a6cfe5ad0a7cd844e47e89e814b6803
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-05-11 15:01:41 -0600

    i work for some functions and two conditions with loops

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -1,19 +1,35 @@
 // *Variables*
 // Declare a variable, assign it a boolean, and alert the value
-
+let isTrue = true;
+alert(isTrue);
 
 // Declare a variable, reassign it to your favorite color, and console log the value
-
+let favColor = "blue";
+favColor = "red";
+console.log (favColor);
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
-
+function numbers (n1, n2, n3, n4){
+    return n1 + n2 + n3 /n4
+}
+numbers(10,20,30,40)
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
-
+function takes (n1,n2){
+    console.log( Math.pow(n1, n2))
+}
+takes (10,20)
 
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+function alertOrLog (b,stra){
+    if (b){
+        alert (str)
+    }else {
+        console.log(str)
+    } 
+}
 
 
 //*Loops*
