43dcbb80a21e3930511f75c099657505d8141ce7
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-05-15 15:37:57 -0600

    i add loops

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,15 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+let movies = ["The Matrix", "Friends", "Mario", "Sonic"]
 
+for (let i = 0; i < movies.length; i++){
+    document.querySelector("h2").textContent += movies[i]
+}
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+//Create an array of numbers. Loop through the array add three to each number and replace the old number.
+let numbers = [1,2,3,4,5,6,7,8,9,10]
 
+for (let i = 0; i < numbers.length; i++){
+    numbers[i] += 3
+}
 
 //Find the average of all the numbers from question three
--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,11 @@
 //Create an array of movies with at least three movies.
-
+let movies = ["The Matrix", "Friends", "Mario", "Sonic"]
 //Using the array from above, store the first movie in a variable
-
+let firstMovie = movies[0]
 //Get the length of the original array and store it in a new variable
-
+let nums = movies.length
+console.log(movies)
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+
+let lastMovie = movies[movies.length - 1]
+
