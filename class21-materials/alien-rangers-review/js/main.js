2464dd2f7a5044d25d004858a507cbae972af8b7
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-13 18:08:58 -0600

    i did objects

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,21 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+let power = ['friends', 'heroes', 'vaseline']
+    
+    power.forEach(show => console.log( power ))
 
 //Create and array of numbers
+let nums = [22, 13, 14, 15, 24, 89, 20]
 //Return a new array of numbers that includes every even number from the previous Arrays
+let onlyEvens = arr => arr.filter( n => n % 2 === 0)
+
+console.log(onlyEvens (nums))
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function takes  (arr){
+    let sorted = arr.sort((a,b) => a-b)
+    alert( sorted[1] + sorted[ sorted.length - 2] )
+}
+takes ([2, 3, 5, 1, 3])
--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,21 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    color: "black";
+    brand: "rolex";
+    minus: "fourth";
+    opacity: 60%;
+}
+
+stopwatch.mi = function (time){
+    console.log(time)
+}
+stopwatch.juanito = function (alarm){
+    console.log(alarm)
+}
+stopwatch.movements = function (bloodPressure){
+    console.log(bloodPressure)
+}
+
+stopwatch.mi(1,2,3,4,5,6,7,8)
+stopwatch.juanito("10:20")
+stopwatch.movements("low-Pressure")
