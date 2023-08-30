5878642f97836f08346a28727f31c622b30d9312
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-08-30 17:15:07 -0600

    i did some objects with methods

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,70 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse = {}
+
+// these are properties of the object mouse 
+
+mouse.brand = 'logitech'
+mouse.color = 'black'
+mouse.model = '123xf'
+
+// Methods of the mouse 
+
+mouse.leftClick= function (){
+    console.log('left click')
+}
+mouse.rightClick = function (){
+    console.log('right click')
+}
+mouse.scroll = function (){
+    console.log ('click, click')
+}
+
+
+
+// THIS is another object and this will be mouse2
+
+let mouse2 = {}
+
+// these are properties of the object mouse 
+
+mouse2.brand = 'logitech'
+mouse2.color = 'black'
+mouse2.model = '456ff'
+
+// Methods of the mouse 
+
+mouse2.leftClick= function (){
+    console.log('left click')
+}
+mouse2.rightClick = function (){
+    console.log('right click')
+}
+mouse2.scroll = function (){
+    console.log ('click, click')
+}
+
+
+
+
+// THIS is another object and this will be mouse3
+
+let mouse3 = {}
+
+// these are properties of the object mouse 
+
+mouse3.brand = 'apple'
+mouse3.color = 'black'
+mouse3.model = 'track2'
+
+// Methods of the mouse 
+
+mouse3.leftClick= function (){
+    console.log('left click')
+}
+mouse3.rightClick = function (){
+    console.log('right click')
+}
+mouse3.scroll = function (){
+    console.log ('click, click')
+}
