5331ac1b7593fea51849e347857e4104a6c0890b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-08-29 18:33:14 -0600

    i did excersises

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -2,7 +2,7 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 let nums = [1, 2, 3, 4]
-    let sum = nums.reduce((acc,c) = > acc + c, 0)
+    let sum = nums.reduce((acc,c) => acc + c, 0)
     alert(sum)
 
 //Create a function that takes in an array of numbers
@@ -13,3 +13,5 @@ let nums = [1, 2, 3, 4]
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+
