68be13d84e2bc52625679f0af430e4f7c3a85e65
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-08-30 17:45:06 -0600

    i did function construction

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,20 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeSFCharacter (punch, kick, catchPhrase, specialMove){
+    this.punch = punch
+    this.kick = kick
+    this.catchPhrase = catchPhrase
+    this.specialMove = specialMove
+    this.taunt = function () {
+    console.log (`You cant handle my ${this.specialMove}`)
+    }
+    this.winning = function () {
+    console.log (`Haha! ${this.catchPhrases}`)
+    }
+    this.dash = function (){
+        console.log('missed me')
+    }
+}
+
+let fighter1 = new MakeSFCharacter ('stron', 'skinny', 'rubeus' '1.20')
+
+let fighter2 = new MakeSFCharacter ('super', 'skinny', 'adults' '1.70')
