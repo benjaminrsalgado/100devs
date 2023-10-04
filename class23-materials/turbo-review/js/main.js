6f33031133e1da5d4e280048c5b5bb53fc74215f
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-04 16:43:46 -0600

    i started working with the excersices

--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -1,10 +1,20 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
-
+let ben = 
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
+function game (){
+    let random = Math.random()
+    if (random < .33){
+        return rock
+    }else if (random < .66){
+        return paper
+    }else{
+        return scissors
+    }
+}
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
