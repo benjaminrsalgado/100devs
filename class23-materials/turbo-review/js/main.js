ae48e2c151a73291362cb8856ec11b31b11bbc77
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-05 12:53:26 -0600

    i did changes

--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -1,23 +1,40 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
-let ben = 
+let ben = 'hello world?'
+alert(ben.endsWith('?'))
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+let str = ' I am looking for jr.dev position.'
+console.log( str.replaceAll('jr.dev', 'software engineer'))
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
-function game (){
+function game(){
     let random = Math.random()
     if (random < .33){
-        return rock
+        return 'rock'
     }else if (random < .66){
-        return paper
+        return 'paper'
     }else{
-        return scissors
+        return 'scissors'
     }
 }
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+function takes (playerChoice){
+    let botChoice = game()
+    if ((playerChoice === 'rock' && botChoice === 'scissors') || 
+    (playerChoice === 'scissors' && botChoice === 'paper') || 
+    (playerChoice === 'paper' && botChoice === 'rock'))
+    console.log('Winner!!')
+    
+}else if (playerChoice === botChoice){
+    console.log('TIIIIIEEEED')
+}else{
+    console.log('YOU LOSE!!!')
+}
+
+takes('paper')
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
