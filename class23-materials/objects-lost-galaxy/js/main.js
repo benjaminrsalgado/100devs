4d00c0a46f770fbf1f1b872161af93184e31146d
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-06 13:49:17 -0600

    i did function construction

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+let dog = {}
+
+dog.size = 12.5
+dog.color = "black"
+dog.age = 10
+dog.name = "frank"
+
+dog.said = function (talk){
+    console.log(`i need to eat ${talk}`)
+}
+
+dog.play = function (){
+    console.log (" need to walk")
+}
+
+dog.couple = function (){
+    console.log(" i need a boyfriend")
+}
--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,20 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeTony (color, size, name, sex){
+    this.aspect = color
+    this.tallest = size
+    this.howis = name
+    this.is=sex
+}
+this.said = function (){
+    alert("this is the figther of the year")
+}
+
+this.place = function (){
+    alert("this event will take in las vegas")
+}
+
+this.president = function (){
+    alert("the president will be Trump")
+}
+
+let benja = new MakeTony("brown", 1.19, "max", "male")
