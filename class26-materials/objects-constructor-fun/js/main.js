818c99f095564b357125730d6981e12c3be9ceda
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-18 14:39:50 -0600

    i did objects

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,40 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizzamachine(source, size, color, taste) {
+    this.source = source;
+    this.size = size;
+    this.color = color;
+    this.taste = taste;
+
+    this.eat = function () {
+        console.log("ljfljdsfjlñdsjfl")
+    }
+    this.go = function () {
+        console.log("I want a pizza " + this.size)
+    }
+    this.went = function () {
+        console.log("jfdsjfsl")
+    }
+}
+
+let pizza1 = new Pizzamachine("chocolate", "25cm", "green", "strawberry")
+
+// New syntaxis of function construction
+
+class MakeCar{
+    constructor(carMake,carModel,carColor,numOfDoors){
+      this.make = carMake
+      this.model = carModel
+      this.color = carColor
+      this.doors = numOfDoors
+    }
+    honk(){
+      alert('BEEP BEEP FUCKER')
+    }
+    lock(){
+      alert(`Locked ${this.doors} doors!`)
+    }
+  }
+  
+  let hondaCivic = new MakeCar('Honda','Civic','Silver', 4)
+  
+  let teslaRoadster = new MakeCar('Tesla','Roadster', 'Red', 2)
--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,36 @@
 //Create a dog object that has four properties and three methods
+
+// Function construction
+function Dog (name, age, breed, color) {
+this. name = name;
+this. age = age;
+this. breed = breed;
+this. color = color;
+this. bark = function() {
+    console.log(this.name + " barks");
+}
+this . eat = function() {
+    console.log(this.name + " eats");
+}
+this . sleep = function() {
+    console.log(this.name + " sleeps");
+}
+}
+
+let dog1 = new Dog("Buddy", 2, "Labrador", "Black");
+let dog2 = new Dog("tay", 5, "Labrador", "White");
+
+// Literal notation 
+
+let pizza = {}
+
+pizza.size = "large";
+pizza.topping = "pepperoni";
+pizaa.price = 10.99;
+
+pizza.size = function() {
+    console.log("The size of the pizza is " + this.size);
+}
+pizza.location = function() {
+    console.log("The location of the pizza is " + this.location);
+}
