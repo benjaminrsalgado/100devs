1351c090064e0b8a45872dc2184b81daa746d355
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-23 12:37:07 -0600

    i add a function in the APPI in order to the user can submit the name of the drink with spaces

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,9 +1,13 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink)
 
+
 function getDrink(){
     let drink = document.querySelector('input').value
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    const encodedIngredient = encodeURIComponent(drink);
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${encodedIngredient}`)
+
     .then(res => res.json()) // parse response as JSON
     .then(data => {
     console.log(data.drinks[0])
