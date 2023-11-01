736c34301cd20754c4ea60a9259fdf0bf06264fc
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-11-01 15:36:41 -0600

    i did changes in searching the book app

--- class28-materials/api-template/js/main.js
+++ class28-materials/api-template/js/main.js
@@ -1,24 +1,28 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+document.querySelector('h2').innerText = localStorage.getItem('books')
+
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+  const url = `https://openlibrary.org/isbn/${choice}.json`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
+        console.log(data.title)
+        if( !localStorage.getItem('books')){
+          localStorage.setItem('books', data.title)
         }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
+        let books = localStorage.getItem('books') + " ; " + data.title
+        localStorage.setItem('books', books)
         }
-       
-        document.querySelector('h3').innerText = data.explanation
+        //put title in local storage
+        
+        document.querySelector('h2').innerText = localStorage.getItem('books') //el += aqui hace que se concatenen el nombre del libro o se salga HaryyHarryHarryHa
+        
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
