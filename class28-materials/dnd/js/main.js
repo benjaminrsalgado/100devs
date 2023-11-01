fb3304844af0a18598da53e2379776a844fb8f53
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-11-01 13:57:12 -0600

    i did an api app

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -7,10 +7,20 @@ function getFetch(){
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
+        .then(data => {
+        console.log(data.subclasses)
+
+        data.subclasses.forEach(element => {
+            console.log(element.name)//element puede ser llamada como sea
+            //create an li
+            const li = document.createElement('li') //se crea un nuevo elemento li 
+            //add text in li
+            li.textContent = element.name //put the name of the subclass in the li 
+            //apend the li to the ul
+            document.querySelector('ul').appendChild(li) // everything que salga en li se va a poner en la ul 
+        })  
       })
-      .catch(err => {
+       .catch(err => {
           console.log(`error ${err}`)
       });
 }
