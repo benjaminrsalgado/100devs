016629b79e4ba1ffa1ddadb37029eb6fc921cd9a
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-11-14 16:53:02 -0600

    i did a class object

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,19 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+class Machine {
+    constructor(machineMake, machineModel, machineColor, machineSize) {
+        this.make = machineMake;
+        this.model = machineModel;
+        this.color = machineColor;
+        this.size = machineSize;
+    }
+
+    first() {
+        alert('¡Hola mundo!');
+    }
+
+    lock() {
+        alert(`Este es el mejor ${this.model} en el mundo`);
+    }
+}
+
+let firstOne = new Machine('muchas', 'super45', 'negro', '2.15cm');
