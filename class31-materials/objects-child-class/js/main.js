ca3d463c8256bcfa196e11db9943cf319e355d97
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-11-16 15:49:00 -0600

    i did a inheritance code

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,29 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this.breed = breed
+    } 
+}
+
+class Cat extends Animal{
+    constructor(name, breed){
+        super(name) //Aqui se accede name a la clase primera que es Animal 
+        this.breed = breed
+    }
+    speak(){
+        console.log('MEOOOWW')
+    }
+}
+
+let simba = new Dog('Simba', 'Sheperd')
+let hola = new Cat('taylor', 'fenix')
