d93660d75b0c80f52828993bd3c114b4b389e4e7
Benjamin Salgado <benjaminrsalgado@gmail.com>
2024-01-18 14:01:03 -0600

    i did some excersices js

--- super-review-JS/review-in-paradise-after-paradise/js/main.js
+++ super-review-JS/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,12 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function takes (arr){
+    if(arr[0]< arr[arr.length -1]){
+        alert('Hi')
+    }else if(arr [0] > arr[arr.leng -1]){
+        alert('bye')
+    }else{
+        alert('we clouse in an hour')
+    }
+}
+
+takes([1,2,3,4,5])
