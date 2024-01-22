54fa7818d56241fde7a0977efcbfe07e13af1228
Benjamin Salgado <benjaminrsalgado@gmail.com>
2024-01-22 12:40:24 -0600

    i did some excersices of java script

--- super-review-JS/objects-dog-photos/js/main.js
+++ super-review-JS/objects-dog-photos/js/main.js
@@ -1,10 +1,10 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
 // Hacer una solicitud GET a una API
-fetch('https://images.dog.ceo/breeds/corgi-cardigan/n02113186_8932.jpg')
+fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then (data => {
         console.log(data.message)
-        document.querySelector('img').src
+        document.querySelector('img').src = data.messsage 
     })
     .catch(err => {
         console.log(`error ${err}`)
