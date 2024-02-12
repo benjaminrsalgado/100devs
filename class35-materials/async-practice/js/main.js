71141835d8398fe6e8a66c7dac08e84f121f2f9d
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-02-12 14:56:34 -0600

    i did excersices

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -1,74 +1,76 @@
 //Code 01
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(){
-//     console.log('Paper delivered to house 2')
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo()
-// houseThree()
+    function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+    function houseTwo(){
+    console.log('Paper delivered to house 2')
+}
+    function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo()
+houseThree()
+
+
 
 //Code 02
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(){
-//     setTimeout(() => console.log('Paper delivered to house 2'), 3000)
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo()
-// houseThree()
+function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+function houseTwo(){
+    setTimeout(() => console.log('Paper delivered to house 2'), 3000)
+}
+function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo()
+houseThree()
 
 //Code 03
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(callback){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 2')
-//         callback()
-//     }, 3000)
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo(houseThree)
+function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+function houseTwo(callback){
+    setTimeout(() => {
+    console.log('Paper delivered to house 2')
+    callback()
+    }, 3000)
+}
+function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo(houseThree)
 
 //Code 04
-// function houseOne(){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 1')
-//         setTimeout(() => {
-//             console.log('Paper delivered to house 2')
-//             setTimeout(() => {
-//                 console.log('Paper delivered to house 3')
-//             }, 3000)
-//         }, 4000)
-//     }, 5000)
-// }
-// houseOne()
+        function houseOne(){
+    setTimeout(() => {
+    console.log('Paper delivered to house 1')
+    setTimeout(() => {
+    console.log('Paper delivered to house 2')
+            setTimeout(() => {
+                console.log('Paper delivered to house 3')
+        }, 3000) ///house three
+       }, 4000) //house two
+  }, 5000) //house one
+ }
+houseOne()
 
 //Code 05
-// const promise = new Promise((resolve, reject) => {
-//     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
-//     }else{
-//         reject('Error: Operation has failed')
-//     }
-// })
-// console.log(promise)
-// promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+const promise = new Promise((resolve, reject) => {
+     const error = false
+     if(!error){
+         resolve('Promise has been fullfilled')
+     }else{
+         reject('Error: Operation has failed')
+    }
+ })
+ console.log(promise)
+ promise
+     .then(data => console.log(data))
+     .catch(err => console.log(err))
 
 //Code 06
 // function houseOne(){
